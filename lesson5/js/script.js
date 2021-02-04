37eb45e40c3137f1acd9f925295d136384cc1403
dsilfvast <dustin@silfvast.com>
2021-02-04 02:07:41 -0700

    Update for week 05

---
+++ lesson5/js/script.js
@@ -0,0 +1,44 @@
+// JavaScript source code
+
+let hambutton;
+let mainnav;
+
+function setScreenWidth(){
+//    let width = window.innerWidth - 50;
+//    document.getElementsByTagName("body")[0].style.width = String(width) + "px";
+}
+
+
+function startPage() {
+    GetModifiedDate();
+//  setScreenWidth();
+    hambutton = document.querySelector('.ham');
+    mainnav = document.querySelector('.navigation')
+
+    hambutton.addEventListener('click', () => { mainnav.classList.toggle('responsive')}, false);
+    // To solve the mid resizing issue with responsive class on
+    window.onresize = () => { if (window.innerWidth > 760) mainnav.classList.remove('responsive'); setScreenWidth()};
+
+}
+
+function GetModifiedDate() {
+
+
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+    let today = new Date();
+    let modifiedDate = new Date(Date.parse(document.lastModified));
+
+    let output = weekDays[today.getDay()];
+    output += ", ";
+    output += String(today.getDate());
+    output += " " + months[today.getMonth()];
+    output += " " + today.getFullYear();
+
+    document.getElementById("year").innerHTML = modifiedDate.getFullYear();
+    document.getElementById("dateTime").innerHTML = output;
+}
+
+
+
