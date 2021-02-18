0e10ad0488ccbd9647e7c71ec3d2910b96f167e5
dsilfvast <dustin@silfvast.com>
2021-02-17 22:34:42 -0700

    lazy loading update

--- lesson7/js/lazy.js
+++ lesson7/js/lazy.js
@@ -17,7 +17,7 @@ const imgOptions = {
 
 const imgObserver = new IntersectionObserver((entries, imgObserver) => {
     entries.forEach(entry => {
-        if (!entry.isIntersecting || entry == null || entry == undefined) return;
+        if (!entry.isIntersecting || entry === null || entry === undefined) return;
 
         preloadImage(entry.target);
         imgObserver.unobserve(entry.target);
