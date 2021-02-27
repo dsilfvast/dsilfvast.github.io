4bc517c916632d33fcf8655988a47a15fe9894a1
dsilfvast <dustin@silfvast.com>
2021-02-26 17:02:45 -0700

    Storm Center update

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -33,8 +33,41 @@ function getModifiedDate() {
     document.getElementById("dateTime").innerHTML = output;
 }
 
+
+//////////////////////////
+// used for form handling
+
 function setAutoFocus(ID) {
-    document.getElementById(ID).focus();
+    console.log(document.getElementById(ID).focus());
+}
+
+function adjustSeverity(rating, targetID) {
+    document.getElementById(targetID).innerHTML = rating;
+    document.getElementById(targetID).style.fontSize = String(1.2 + parseInt(rating) * 0.1) + "rem";
 }
 
 
+
+function insertName(ID) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const fullName = urlParams.get('full-name');
+    document.getElementById(ID).innerHTML = ", " + fullName;
+}
+
+function formReport(ID) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let output = "<strong>Name: </strong>" + urlParams.get('full-name') + "<br />";
+    output += "<strong>Email: </strong>" + urlParams.get('email') + "<br />";
+    output += "<strong>Phone: </strong>" + urlParams.get('phone') + "<br />";
+    output += "<strong>Zip Code: </strong>" + urlParams.get('zipcode') + "<br />";
+    output += "<strong>Storm Date: </strong>" + urlParams.get('stormdate') + "<br />";
+    output += "<strong>Storm Type: </strong>" + urlParams.get('stormtype') + "<br />";
+    output += "<strong>Severity: </strong>" + urlParams.get('severity') + "<br />";
+    output += "<strong>Region: </strong>" + urlParams.get('stormregion') + "<br />";
+    output += "<strong>Danger: </strong>" + urlParams.get('danger') + "<br />";
+    output += "<strong>Comment: </strong>" + urlParams.get('comment') + "<br />";
+
+    document.getElementById(ID).innerHTML = output;
+}
