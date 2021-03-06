6cef1175d475e8b50ab4a8e8c0335aab7f83678c
dsilfvast <dustin@silfvast.com>
2021-03-05 21:46:34 -0700

    Update to main weather page

--- lesson9/js/cities.js
+++ lesson9/js/cities.js
@@ -1,29 +1,47 @@
 // JavaScript source code
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+function loadCity(cities, cityName) {
+    for (let i = 0; i < cities.length; i++) {
+        if (cityName == cities[i].name) {
+            let city = document.createElement('section');
+            let h3 = document.createElement('h3');
+            let mottoP = document.createElement('p');            
+            let foundedDiv = document.createElement('div');
+            let populationDiv = document.createElement('div');
+            let rainDiv = document.createElement('div');
+            let statsDiv = document.createElement('div');
+            let image = document.createElement('img');
+
+            h3.textContent = cities[i].name;
+            mottoP.textContent = cities[i].motto;
+            foundedDiv.textContent = "Year Founded: " + cities[i].yearFounded;
+            populationDiv.textContent = "Population: " + cities[i].currentPopulation;
+            rainDiv.textContent = "Annual Rain Fall: " + cities[i].averageRainfall;
+            image.setAttribute('src', "images/" + cities[i].photo);
+            image.setAttribute('alt', cities[i].name);
+
+            statsDiv.appendChild(foundedDiv);
+            statsDiv.appendChild(populationDiv);
+            statsDiv.appendChild(rainDiv);
+
+            city.appendChild(h3);
+            city.appendChild(mottoP);
+            city.appendChild(statsDiv);
+            city.appendChild(image);
+            document.querySelector('div.cities-div').appendChild(city);
+        }
+    }
+}
+
 fetch(requestURL)
     .then(function (response) {
         return response.json();
     })
     .then(function (jsonObject) {
-        console.table(jsonObject);  // temporary checking for valid response and data parsing
-        const prophets = jsonObject['prophets'];
-        for (let i = 0; i < prophets.length; i++) {
-            let card = document.createElement('section');
-            let h2 = document.createElement('h2');
-            let birthDate = document.createElement('div');
-            let birthPlace = document.createElement('div');
-            let image = document.createElement('img');
-
-            h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-            birthDate.textContent = "Date of Birth: " + prophets[i].birthdate;
-            birthPlace.textContent = "Place of Birth: " + prophets[i].birthplace;
-            image.setAttribute('src', prophets[i].imageurl);
-            image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + " - " + prophets[i].order);
-
-            card.appendChild(h2);
-            card.appendChild(birthDate);
-            card.appendChild(birthPlace);
-            card.appendChild(image);
-            document.querySelector('div.cards').appendChild(card);
-        }
+        //console.table(jsonObject);  // temporary checking for valid response and data parsing
+        const cities = jsonObject['towns'];
+        loadCity(cities, "Preston");
+        loadCity(cities, "Soda Springs");
+        loadCity(cities, "Fish Haven");
     });
