f3d3de72f35123725c91de0d5599b734032359ae
dsilfvast <dustin@silfvast.com>
2021-04-05 08:30:14 -0600

    update directory js

--- taylorsville/js/directory.js
+++ taylorsville/js/directory.js
@@ -7,7 +7,7 @@ fetch(requestURL)
     .then(function (jsonObject) {
         // console.table(jsonObject);  // temporary checking for valid response and data parsing
         const businesses = jsonObject['directory'];
-        for (let i = 0; i < prophets.length; i++) {
+        for (let i = 0; i < businesses.length; i++) {
             let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let image = document.createElement('img');
